24f4ecd429e080129746d807ead5f1555a4e96e0
Chandra Prakash Khanal <132903713+CPKhanal@users.noreply.github.com>
2025-02-28 19:45:20 -0500

    removed the bug

--- manage-cart.php
+++ manage-cart.php
@@ -1,58 +1,66 @@
 <?php
 session_start();
-// session_destroy();
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST['Add_To_Cart'])) {
-        if (isset($_SESSION['cart'])) {
-            $myitems = array_column($_SESSION['cart'], 'Item_Name');
-            if (in_array($_POST['Item_Name'], $myitems)) {
-                echo "<script>
-            alert('Item Already Added') 
-            window.location.href='index.php';
-            </script>";
-            } else {
-                $count = count($_SESSION['cart']);
-                $_SESSION['cart'][$count] = array('Item_Name' => $_POST['Item_Name'], 'Price' => $_POST['Price'], 'Quantity' => 1);
-                //    print_r($_SESSION['cart']);
-                echo "<script>
-            alert('Item Added') 
-            window.location.href='index.php';
-            </script>";
-            }
-        } else {
-            $_SESSION['cart'][0] = array('Item_Name' => $_POST['Item_Name'], 'Price' => $_POST['Price'], 'Quantity' => 1);
-            echo "<script>
-            alert('Item Added') 
-            window.location.href='index.php';
-            </script>";
-        }
+// Function to add item to cart
+function addToCart($itemName, $price)
+{
+    if (!isset($_SESSION['cart'])) {
+        $_SESSION['cart'] = [];
     }
 
-    if(isset($_POST['Remove_Item'])){
-        foreach($_SESSION['cart'] as $key => $value){
-            if($value['Item_Name']==$_POST['Item_Name']){
+    // Check if item already exists in cart
+    $itemIndex = array_search($itemName, array_column($_SESSION['cart'], 'Item_Name'));
+
+    if ($itemIndex !== false) {
+        echo "<script>alert('Item Already Added'); window.location.href='index.php';</script>";
+    } else {
+        $_SESSION['cart'][] = [
+            'Item_Name' => htmlspecialchars($itemName),
+            'Price' => floatval($price),
+            'Quantity' => 1
+        ];
+        header("Location: index.php");
+        exit();
+    }
+}
+
+// Function to remove item from cart
+function removeItem($itemName)
+{
+    foreach ($_SESSION['cart'] as $key => $item) {
+        if ($item['Item_Name'] === $itemName) {
             unset($_SESSION['cart'][$key]);
-            $_SESSION['cart']=array_values($_SESSION['cart']);
-            echo"
-            <script>
-                alert('Item Removed from Cart');
-                window.location.href='mycart.php';
-            </script>";
-            }
+            $_SESSION['cart'] = array_values($_SESSION['cart']); // Re-index array
+            header("Location: mycart.php");
+            exit();
         }
     }
+}
 
-    if(isset($_POST['Mod_Quantity'])){
-        foreach($_SESSION['cart'] as $key => $value){
-            if($value['Item_Name']==$_POST['Item_Name']){
-                $_SESSION['cart'][$key]['Quantity']=$_POST['Mod_Quantity'];
-            echo"
-            <script>
-                window.location.href='mycart.php';
-            </script>";
-            }
+// Function to modify quantity
+function modifyQuantity($itemName, $newQuantity)
+{
+    foreach ($_SESSION['cart'] as $key => $item) {
+        if ($item['Item_Name'] === $itemName) {
+            $_SESSION['cart'][$key]['Quantity'] = intval($newQuantity);
+            header("Location: mycart.php");
+            exit();
         }
+    }
+}
+
+// Handling POST Requests
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    if (isset($_POST['Add_To_Cart'])) {
+        addToCart($_POST['Item_Name'], $_POST['Price']);
+    }
+
+    if (isset($_POST['Remove_Item'])) {
+        removeItem($_POST['Item_Name']);
+    }
+
+    if (isset($_POST['Mod_Quantity'])) {
+        modifyQuantity($_POST['Item_Name'], $_POST['Mod_Quantity']);
     }
 }
 ?>
