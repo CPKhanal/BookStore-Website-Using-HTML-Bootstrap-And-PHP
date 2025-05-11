1ea07e00090a9f232f59ecf3a94ba7283a726893
Chandra Prakash Khanal <132903713+CPKhanal@users.noreply.github.com>
2025-05-10 22:59:20 -0400

    added sql query

--- SQL Query For Database/bookstore.sql
+++ SQL Query For Database/bookstore.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 04, 2025 at 12:41 AM
+-- Generation Time: May 11, 2025 at 04:57 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -38,7 +38,8 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `username`, `password`) VALUES
-(1, 'admin', '$2y$10$mBHNyNHdu.t9sdHMLI2Wyu7l93BeUTfPi4OO8smJ1myvgaDCd.xPC');
+(1, 'admin', '$2y$10$mBHNyNHdu.t9sdHMLI2Wyu7l93BeUTfPi4OO8smJ1myvgaDCd.xPC'),
+(3, 'admin@admin.com', '$2y$10$cChJt0vOvypwnVGERxezmuF2T5mceZtNMEF7BUZv3uZCEd/2zH7hG');
 
 -- --------------------------------------------------------
 
@@ -81,10 +82,37 @@ CREATE TABLE `announcement_replies` (
 --
 
 INSERT INTO `announcement_replies` (`reply_id`, `announcement_id`, `user_id`, `reply_text`, `created_at`) VALUES
-(16, 11, NULL, 'hi', '2025-03-03 23:29:44'),
-(17, 11, NULL, 'hey', '2025-03-03 23:32:29'),
-(18, 11, NULL, 'hey', '2025-03-03 23:32:46'),
-(19, 11, NULL, 'hii', '2025-03-03 23:33:17');
+(30, 11, 198, 'hi How are you', '2025-03-04 00:03:35'),
+(31, 11, NULL, 'good', '2025-03-04 00:06:14'),
+(32, 11, 198, 'wow', '2025-03-04 00:06:25'),
+(33, 11, 197, 'Whats happening', '2025-03-04 00:14:37'),
+(34, 11, NULL, 'hi cp', '2025-03-04 06:20:26'),
+(35, 11, 197, 'hi boss', '2025-03-04 06:20:53'),
+(36, 11, 200, 'this is test2', '2025-03-04 06:23:46'),
+(37, 11, 202, 'ok doing great', '2025-03-04 07:15:03'),
+(38, 11, 202, 'ok', '2025-03-04 07:15:42'),
+(39, 11, 202, 'reply', '2025-03-04 07:16:54'),
+(42, 11, NULL, 'hi', '2025-03-06 19:21:47');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`id`, `title`, `price`, `image`) VALUES
+(7, 'Elon Musk By Walter Isaacson', 35.00, 'elon musk.jpg');
 
 -- --------------------------------------------------------
 
@@ -110,7 +138,12 @@ INSERT INTO `contactus` (`id`, `name`, `email`, `message`, `date`) VALUES
 (16, 'cp khanal', 'test1@gmail.com', 'hi nice to meet you\r\n', '2025-03-01 00:47:45'),
 (17, 'Chandra Prakash Khanal', 'cpkhanal0@gmail.com', 'Hi I would like to talk to you!\r\n', '2025-03-03 03:12:28'),
 (18, 'CP', 'cpkhanal0@gmail.com', 'This is a test to check if profile is working or not', '2025-03-03 19:52:45'),
-(19, 'Ankit', 'ankitpoudel@gmail.com', 'This a a message form ankit', '2025-03-03 20:46:06');
+(19, 'Ankit', 'ankitpoudel@gmail.com', 'This a a message form ankit', '2025-03-03 20:46:06'),
+(20, 'test2', 't2st@gmail.com', 'test2', '2025-03-04 06:25:42'),
+(21, 'test2', 'test2@gmail.comm', 'hi', '2025-03-04 06:26:03'),
+(22, 'test2', 'test2@gmail.com', 'Hi how are you', '2025-03-04 06:26:31'),
+(23, 'test3', 'test3@gmail.com', 'testing 3', '2025-03-04 06:53:23'),
+(24, 'test3', 'test3@gmail.com', 'this is test of db conn', '2025-03-04 07:31:14');
 
 -- --------------------------------------------------------
 
@@ -146,7 +179,13 @@ INSERT INTO `order_manager` (`Order_Id`, `Full_Name`, `Phone_No`, `Address`, `Pa
 (19, 'cp', 4376602672, '33 city center dr', 'COD'),
 (20, 'test1', 1111, 'test1', 'COD'),
 (21, 'test1', 111222333, '12345', 'COD'),
-(22, 'Ankit', 123, '33 city center dr', 'COD');
+(22, 'Ankit', 123, '33 city center dr', 'COD'),
+(23, 'test2', 123, '123', 'COD'),
+(24, 'ok', 11, '11', 'COD'),
+(25, 'cp', 222222, '22', 'COD'),
+(26, 'test4', 11, 'ss', 'COD'),
+(27, 'CP Khanal', 4376602672, '593 Rossellini Drivr', 'COD'),
+(28, 'cp', 12, 'as', 'COD');
 
 -- --------------------------------------------------------
 
@@ -170,7 +209,10 @@ INSERT INTO `userdetails` (`id`, `email`, `name`, `password`, `created_at`) VALU
 (196, 'boss@gmail.com', 'CP', '$2y$10$al3NBx2ozB8SWVJYzg808OunWtvu7xic8UVH4E2GaOhpGbGkiPT7O', '2025-03-01 02:39:54'),
 (197, 'cpkhanal0@gmail.com', 'CP', '$2y$10$0CMQhDY4z.T5FCBl.zJojORleuHmDDcLMzKNBnzP/E4wiiWkKNey2', '2025-03-03 19:48:37'),
 (198, 'test1@gmail.com', 'test1', '$2y$10$eujA4v8jRQiRNsSeBSxnaeiiBQgjvM5cpCTxvtEr8x.wGbAxuQxQu', '2025-03-03 19:54:51'),
-(199, 'ankitpoudel@gmail.com', 'Ankit', '$2y$10$a40fhSroPcdYmArldDQibu3v/0Dl3ErGnTynjBhRVo0wigQDrgCC.', '2025-03-03 20:45:07');
+(199, 'ankitpoudel@gmail.com', 'Ankit', '$2y$10$a40fhSroPcdYmArldDQibu3v/0Dl3ErGnTynjBhRVo0wigQDrgCC.', '2025-03-03 20:45:07'),
+(200, 'test2@gmail.com', 'test2', '$2y$10$bI.Pg5arebg1bi19bvJiOOu5CsA.BEj5GKqYNsClpkz5qFK6/qdeC', '2025-03-04 06:22:43'),
+(202, 'test3@gmail.com', 'test3', '$2y$10$xZWUvkvfigU9uqwxkPnhwOMO.kdHMVXovdC9f9ZRDi7XJCyaVqKHy', '2025-03-04 06:58:48'),
+(203, 'test4@gmail.com', 'test4', '$2y$10$ceaRpylFBKL9TtRuMfphAu8N2gmaxUYjBKRv27IeDzWo64l6K1/o.', '2025-03-04 07:32:57');
 
 -- --------------------------------------------------------
 
@@ -228,7 +270,15 @@ INSERT INTO `user_orders` (`Order_Id`, `Item_Name`, `Price`, `Quantity`) VALUES
 (21, 'How to Win Friends and Influence People', 55, 1),
 (21, 'The 48 Laws of Power', 60, 1),
 (21, 'The Magic', 60, 1),
-(22, 'The 48 Laws of Power', 60, 2);
+(22, 'The 48 Laws of Power', 60, 2),
+(23, 'Cant Hurt Me', 40, 1),
+(23, 'Harry Potter', 45, 1),
+(23, 'The Psychology of Money', 50, 1),
+(24, 'Cant Hurt Me', 40, 1),
+(25, 'The Psychology of Money', 50, 1),
+(26, 'Chanakya Neeti', 60, 1),
+(27, 'We Who Wrestle With God', 60, 1),
+(28, 'Elon Musk By Walter Isaacson', 35, 2);
 
 --
 -- Indexes for dumped tables
@@ -256,6 +306,12 @@ ALTER TABLE `announcement_replies`
   ADD KEY `user_id` (`user_id`);
 
 --
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `contactus`
 --
 ALTER TABLE `contactus`
@@ -281,37 +337,43 @@ ALTER TABLE `userdetails`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `announcements`
 --
 ALTER TABLE `announcements`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `announcement_replies`
 --
 ALTER TABLE `announcement_replies`
-  MODIFY `reply_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `reply_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `contactus`
 --
 ALTER TABLE `contactus`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `order_manager`
 --
 ALTER TABLE `order_manager`
-  MODIFY `Order_Id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `Order_Id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `userdetails`
 --
 ALTER TABLE `userdetails`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=200;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=204;
 
 --
 -- Constraints for dumped tables
